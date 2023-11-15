4baf567e20ca4044ce940895c9aa2e3ce7f54448
whoyoux <48690153+whoyoux@users.noreply.github.com>
2023-11-15 23:25:48 +0100

    add basic server http and socket

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-console.log("hello");
+import { Server } from "./server";
+
+async function main() {
+    const server = new Server();
+
+    server.listen((port) => {
+        console.log(`Server is listening on http://localhost:${port}`);
+    });
+}
+
+main().catch(console.error);
---
+++ src/server.ts
@@ -0,0 +1,49 @@
+import express, { Application } from "express";
+import { createServer, Server as HTTPServer } from "http";
+import { Server as SocketIOServer } from "socket.io";
+
+export class Server {
+    private httpServer!: HTTPServer;
+    private app!: Application;
+    private io!: SocketIOServer;
+
+    private readonly DEFAULT_PORT = 5000;
+
+    constructor() {
+        this.initialize();
+
+        this.configureApp();
+
+        this.handleRoutes();
+        this.handleSocketConnection();
+    }
+
+    private initialize(): void {
+        this.app = express();
+        this.httpServer = createServer(this.app);
+        this.io = new SocketIOServer(this.httpServer);
+    }
+
+    private configureApp(): void {
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({ extended: false }));
+    }
+
+    private handleRoutes(): void {
+        this.app.get("/", (req, res) => {
+            res.json({ message: "Hello World" });
+        });
+    }
+
+    private handleSocketConnection(): void {
+        this.io.on("connection", (socket) => {
+            console.log("Socket connected.");
+        });
+    }
+
+    public listen(callback: (port: number) => void): void {
+        this.httpServer.listen(this.DEFAULT_PORT, () =>
+            callback(this.DEFAULT_PORT)
+        );
+    }
+}
